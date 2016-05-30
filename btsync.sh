1fd9a740117800bca26583e525b5c61264c1385a
heavenbird <zhangxinqi2013@gmail.com>
2016-05-30 13:29:14 +0800

    第一版，支持32位和64位，不支持自定义用户名密码、设备名等

---
+++ btsync.sh
@@ -0,0 +1,32 @@
+#sudo wget http://download-lb.utorrent.com/endpoint/btsync/os/linux-x64/track/stable -O /tmp/btsync_x64.tar.gz
+
+function set32 {
+wget https://download-cdn.getsync.com/stable/linux-i386/BitTorrent-Sync_i386.tar.gz -O /tmp/btsync_i386.tar.gz
+cd /opt/btsync
+sudo tar xzf /tmp/btsync_i386.tar.gz
+}
+function set64 {
+wget https://download-cdn.getsync.com/stable/linux-x64/BitTorrent-Sync_x64.tar.gz -O /tmp/btsync_x64.tar.gz
+cd /opt/btsync
+sudo tar xzf /tmp/btsync_x64.tar.gz
+
+}
+
+mkdir /opt/btsync
+mkdir /opt/btsync_transfer
+if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ] ; then
+
+    set64
+
+else
+
+    set32
+
+fi
+cp cp -a /root/btsync/btsync  /etc/init.d/btsync
+chmod +x /etc/init.d/btsync
+cp cp -a /root/btsync/btsync.cfg  /opt/btsync/btsync.cfg
+
+
+#Æô¶¯
+/etc/init.d/btsync start
